8bb5d5f96b153044037532a1911132eaaa1d7323
Ivan Velichko <iximiuz@gmail.com>
2022-11-06 21:39:25 +0000

    Port forwarding WiP

--- cmd/portforward/portforward.go
+++ cmd/portforward/portforward.go
@@ -2,8 +2,10 @@ package portforward
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
@@ -25,50 +27,37 @@ import (
 // Possible options (kinda sorta as in ssh -L):
 //   - TARGET_PORT                                # binds TARGET_IP:TARGET_PORT to a random port on localhost
 //   - TARGET_IP:TARGET_PORT                      # The second form is needed to:
-//                                                #  1) allow target's localhost ports expose
-//                                                #  2) specify a concrete IP if a multi-network target listens on 0.0.0.0
+//                                                #  1) allow exposing target's localhost ports
+//                                                #  2) specify a concrete IP for a multi-network target
 //
 //   - LOCAL_PORT:TARGET_PORT                     # binds TARGET_IP:TARGET_PORT to LOCAL_PORT on localhost
 //   - LOCAL_PORT:TARGET_IP:TARGET_PORT
 //
-//   - LOCAL_IP:LOCAL_PORT:TARGET_PORT            # binds TARGET_IP:TARGET_PORT to LOCAL_PORT on LOCAL_IP
+//   - LOCAL_IP:LOCAL_PORT:TARGET_PORT            # similar to LOCAL_PORT:TARGET_PORT but LOCAL_IP is used instead of localhost
 //   - LOCAL_IP:LOCAL_PORT:TARGET_IP:TARGET_PORT
 
 const (
-	helperImage = "nixery.dev/socat:latest"
+	helperImage = "nixery.dev/shell/socat:latest"
 )
 
 type options struct {
-	target  string
-	address string
-	ports   []string
+	target      string
+	forwardings []string
 }
 
 func NewCommand(cli cmd.CLI) *cobra.Command {
 	var opts options
 
 	cmd := &cobra.Command{
-		Use:   "port-forward [OPTIONS] CONTAINER [LOCAL_PORT:]TARGET_PORT [...[LOCAL_PORT_N:]TARGET_PORT_N]",
-		Short: "Publish a port of an already running container (kind of)",
+		Use:   "port-forward CONTAINER [[LOCAL_IP:]LOCAL_PORT:]TARGET_PORT [...]",
+		Short: `"Publish" one or more ports of an already running container`,
 		Args:  cobra.MinimumNArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			opts.target = args[0]
-			if len(args) > 1 {
-				opts.ports = args[1:]
-			}
+			opts.forwardings = args[1:]
 			return runPortForward(context.Background(), cli, &opts)
 		},
 	}
-
-	flags := cmd.Flags()
-	flags.SetInterspersed(false) // Instead of relying on --
-
-	flags.StringVar(
-		&opts.address,
-		"address",
-		"127.0.0.1",
-		"Host's interface address to bind the port to",
-	)
 
 	return cmd
 }
@@ -91,22 +80,27 @@ func runPortForward(ctx context.Context, cli cmd.CLI, opts *options) error {
 		return err
 	}
 
-	ports, portBindings, err := nat.ParsePortSpecs([]string{"8080:80"})
+	forwardings, err := prepareForwardings(target, opts.forwardings)
 	if err != nil {
 		return err
 	}
 
-	contIP := target.NetworkSettings.Networks["bridge"].IPAddress
+	exposedPorts, portBindings, err := nat.ParsePortSpecs(forwardings.toDockerPortSpecs())
+	if err != nil {
+		return err
+	}
+
+	// TODO: Iterate over all forwardings.
 	resp, err := client.ContainerCreate(
 		ctx,
 		&container.Config{
 			Image:      helperImage,
 			Entrypoint: []string{"socat"},
 			Cmd: []string{
-				"TCP-LISTEN:80,fork",
-				fmt.Sprintf("TCP-CONNECT:%s:%d", contIP, 80),
+				fmt.Sprintf("TCP-LISTEN:%s,fork", forwardings[0].targetPort),
+				fmt.Sprintf("TCP-CONNECT:%s:%d", forwardings[0].targetIP, forwardings[0].targetPort),
 			},
-			ExposedPorts: ports,
+			ExposedPorts: exposedPorts,
 		},
 		&container.HostConfig{
 			AutoRemove:   true,
@@ -124,6 +118,7 @@ func runPortForward(ctx context.Context, cli cmd.CLI, opts *options) error {
 		return fmt.Errorf("cannot start port-forwarder container: %w", err)
 	}
 
+	// TODO: Handle ctrl + C and other signals.
 	forwarderStatusCh, forwarderErrCh := client.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
 	// targetStatusCh, targetErrCh := client.ContainerWait(ctx, target.ID, container.WaitConditionNotRunning)
 	select {
@@ -151,4 +146,83 @@ func pullImage(
 
 	_, err = io.Copy(cli.OutputStream(), resp)
 	return err
+}
+
+type forwarding struct {
+	localIP    string
+	localPort  string
+	targetIP   string
+	targetPort string
+}
+
+type forwardingList []forwarding
+
+func (list forwardingList) toDockerPortSpecs() []string {
+	// ip:hostPort:containerPort | ip::containerPort | hostPort:containerPort | containerPort
+	var spec []string
+	for _, f := range list {
+		spec = append(spec, fmt.Sprintf("%s:%s:%s", f.localIP, f.localPort, f.targetPort))
+	}
+	return spec
+}
+
+func prepareForwardings(
+	target types.ContainerJSON,
+	forwardings []string,
+) (forwardingList, error) {
+	var list forwardingList
+
+	targetIP := target.NetworkSettings.Networks["bridge"].IPAddress
+
+	for _, f := range forwardings {
+		parts := strings.Split(f, ":")
+		if len(parts) == 1 {
+			// Case 1: TARGET_PORT
+
+			if _, err := nat.ParsePort(parts[0]); err != nil {
+				// TODO: Return a user-friendly error.
+				return nil, err
+			}
+
+			// TODO: if "target has more than 1 IP" return err
+
+			list = append(list, forwarding{
+				localIP:    "127.0.0.1",
+				targetPort: parts[0],
+				targetIP:   targetIP,
+			})
+			continue
+		}
+
+		if len(parts) == 2 {
+			if _, err := nat.ParsePort(parts[0]); err == nil {
+				// Case 2: LOCAL_PORT:TARGET_PORT
+
+				// TODO: if "target has more than 1 IP" return err
+
+				list = append(list, forwarding{
+					localPort:  parts[0],
+					localIP:    "127.0.0.1",
+					targetPort: parts[1],
+					targetIP:   targetIP,
+				})
+			} else {
+				// Case 3: TARGET_IP:TARGET_PORT
+
+				// TODO: if "parts[0] not in target IP list" return err
+
+				list = append(list, forwarding{
+					localIP:    "127.0.0.1",
+					targetPort: parts[1],
+					targetIP:   parts[0],
+				})
+			}
+			continue
+		}
+
+		// TODO: other cases
+		return nil, errors.New("implement me")
+	}
+
+	return list, nil
 }
